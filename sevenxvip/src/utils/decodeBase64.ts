2c36f7a88c7c72b9b8fa9d535808ac4092941f5f
RuanDevz <ruanbatista1509@gmail.com>
2025-11-18 15:07:10 -0300

    fix encode

--- sevenxvip/src/utils/decodeBase64.ts
+++ sevenxvip/src/utils/decodeBase64.ts
@@ -1,6 +1,9 @@
 export const decodeBase64 = (base64: string) => {
   try {
-    const decoded = atob(base64);
+    // remove o 3º caractere que foi inserido propositalmente
+    const cleanBase64 = base64.slice(0, 2) + base64.slice(3);
+
+    const decoded = atob(cleanBase64);
     return JSON.parse(decoded);
   } catch (err) {
     console.error("Erro ao decodificar:", err);
